7eb96b70bf8660b8e02b2360b851b1b40a326e74
putrizulfi <80367043+putrizulfi@users.noreply.github.com>
2022-05-24 18:43:37 +0700

    Add files via upload

---
+++ 20090138/other/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostDB = '127.0.0.1';
+$portDB = '5432';
+$nameDB = 'uts';
+$userDB = 'postgres';
+$pwDB = '12345';
+
+$connection = pg_connect("host=$hostDB port=$portDB dbname=$nameDB user=$userDB password=$pwDB");
+session_start();
+if($connection){
+    
+}else{
+    
+}
---
+++ 20090138/profil.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'other/head.php'; ?>
+    <title>Profil</title>
+</head>
+<body style="background-color: #FFEBCD;">
+    <?php include 'other/nav.php'; ?>
+    
+    <div class="container mt-4">
+        <div class="card border-white shadow-sm  mb-4" style="border-radius: 15px;">
+            <div class="card-body"> 
+                <div class="col-12 text-center">
+                    <h3>Profil</h3>
+                    
+                    <hr>
+                    <td><h4>Nama : Putri Zufiaturrizki</h4></td>
+                    <td><h4>Kelas : 4D</h4></td>
+                    <td><h4>NIM : 20090138</h4></td>
+                    <a href="index.php" class="btn btn-danger p-2 mb-3" >Kembali</a>
+                </div>
+                <div class="card-footer bg-white text-center">
+                <a href="https://github.com/putrizulfi" target="_blank"><h3><i class="bi bi-github text-black "></i></h3></a>
+                <p>my github</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'other/js.php'; ?>
+</body>
+</html>
